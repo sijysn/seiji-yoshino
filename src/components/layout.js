e227945484e7995589130a5f8e991cbe30524e26
yoshinoseiji <sij.ysn25@gmail.com>
2022-05-30 23:33:37 +0900

    defaultを削除

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,19 +1,22 @@
 import * as React from "react"
 import { Header } from "./common/Header"
+import styled from "styled-components"
 
-const Layout = ({ location, title, children }) => {
+export const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
 
   return (
     <>
       <Header />
-      <div className="global-wrapper" data-is-root-path={isRootPath}>
+      <Content data-is-root-path={isRootPath}>
         <main>{children}</main>
         <footer>© allrights reserved seiji yoshino</footer>
-      </div>
+      </Content>
     </>
   )
 }
 
-export default Layout
+const Content = styled.div`
+  padding-top: 50px;
+`
