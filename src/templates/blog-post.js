6937dad15e4277f6040f0db6a27499dd77c5bd29
yoshinoseiji <sij.ysn25@gmail.com>
2022-06-08 01:17:49 +0900

    pathを修正

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,7 +1,6 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
 
-import { About } from "../components/About"
 import { Layout } from "../components/Layout"
 import { Seo } from "../components/Seo"
 
